30be6e7395695948d486ad10c2f1f92a946f5845
K1rby <quentin.fontenay@hotmail.com>
2019-11-20 16:05:31 +0100

    page accueil style

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,10 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Accueil from "../views/Accueil.vue";
 import Inscription from "../views/Inscription";
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+Vue.use(VueAxios, axios)
 Vue.use(VueRouter)
 
 const routes = [
--- src/views/Accueil.vue
+++ src/views/Accueil.vue
@@ -1,13 +1,86 @@
 <template>
-<div></div>
+    <div>
+        <v-toolbar dark>
+            <v-toolbar-title>LOCASPORT</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-toolbar-items>
+                <v-btn text>Link 1</v-btn>
+                <v-btn text>Link 2</v-btn>
+                <v-btn text>Link 3</v-btn>
+            </v-toolbar-items>
+
+            <template v-if="$vuetify.breakpoint.smAndUp">
+                <v-btn icon>
+                    <v-icon>mdi-export-variant</v-icon>
+                </v-btn>
+                <v-btn icon>
+                    <v-icon>mdi-delete-circle</v-icon>
+                </v-btn>
+                <v-btn icon>
+                    <v-icon>mdi-plus-circle</v-icon>
+                </v-btn>
+            </template>
+        </v-toolbar>
+        <v-container class="my-5">
+            <v-layout row wrap>
+                <v-flex xs12 sm6 md4 lg3 v-for="equip in equipement" v-bind:key="equip">
+                    <v-card hover class="mx-auto ma-6" max-width="300">
+                        <v-img class="white--text align-end" height="200px" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
+                            <v-card-title>{{equip.name}} de {{equip.sport}}</v-card-title>
+                        </v-img>
+
+                        <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+
+                        <v-card-text class="text--primary">
+                            <div>Whitehaven Beach</div>
+                            <div>Whitsunday Island, Whitsunday Islands</div>
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-btn color="orange">Réserver</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Accueil"
+        name: "Accueil",
+        data: () => ({
+            equipement: [],
+            loading: false,
+            selection: 1,
+        }),
+        methods: {
+            reserve() {
+                this.loading = true
+                setTimeout(() => (this.loading = false), 2000)
+            }
+        },
+        mounted() {
+            this.axios.get('http://localhost:8080/equipement/'
+            )
+                .then((response) => {
+                    this.equipement = response.data
+                }, (error) => {
+                    console.log(error);
+                });
+        }
     }
 </script>
 
 <style scoped>
+    #cardSwag {
+        font-family: 'Open Sans', sans-serif;
 
+        margin-left: 20px;
+        margin-right: 20px;
+
+        display: flex;
+        flex-direction: row;
+    }
 </style>
