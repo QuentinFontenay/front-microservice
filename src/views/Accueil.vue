e8865b83ddda6fec812fcd5115cb0ccbfda38be7
K1rby <quentin.fontenay@hotmail.com>
2019-11-20 17:24:28 +0100

    ajout des images

--- src/views/Accueil.vue
+++ src/views/Accueil.vue
@@ -27,15 +27,12 @@
             <v-layout row wrap>
                 <v-flex xs12 sm6 md4 lg3 v-for="equip in equipement" v-bind:key="equip">
                     <v-card hover class="mx-auto ma-6" max-width="300">
-                        <v-img class="white--text align-end" height="200px" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
-                            <v-card-title>{{equip.name}} de {{equip.sport}}</v-card-title>
-                        </v-img>
+                        <v-img class="white--text align-end" height="200px" :src="require('../assets/ballonBasket.jpg')"></v-img>
 
-                        <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+                        <v-card-subtitle class="pb-0">{{equip.name}} de {{equip.sport}}</v-card-subtitle>
 
                         <v-card-text class="text--primary">
-                            <div>Whitehaven Beach</div>
-                            <div>Whitsunday Island, Whitsunday Islands</div>
+                            <div>{{equip.description}}</div>
                         </v-card-text>
                         <v-card-actions>
                             <v-btn color="orange">Réserver</v-btn>
