054a92fc9ea7172cc2b427845e03ad48ecf18410
K1rby <quentin.fontenay@hotmail.com>
2019-11-20 11:38:44 +0100

    change app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div>
-      <btn @click="clickInscription">Inscription</btn>
-      <btn @click="clickAccueil">Accueil</btn>
-    </div>
+    <v-btn color="success" class="mr-4" @click="clickInscription">Inscription</v-btn>
+    <v-btn color="success" class="mr-4" @click="clickAccueil">Accueil</v-btn>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  },
   methods: {
     clickInscription () {
       this.$router.push({ name: 'Inscription' })
